a6b538b9b1570d19a91f69a8d8adf8006e67cfd7
dbc-hou <david.collins.hou@gmail.com>
2022-03-29 14:18:07 -0500

    sql order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,14 +2,15 @@ use david;
 
 select * from employees
 where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
-and gender = 'M';
+order by last_name, first_name;
 
 select * from employees
-where last_name like 'E%' or last_name like '%E';
+where last_name like 'E%' or last_name like '%E'
+order by emp_no desc;
 
-select first_name,last_name from employees
+select * from employees
 where last_name like 'E%' and last_name like '%E'
-order by hire_date;
+order by emp_no desc;
 
 select * from employees
 where last_name like '%q%';
