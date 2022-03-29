ce6a1046debeb14bac18cd90c272b1623ef7d823
dbc-hou <david.collins.hou@gmail.com>
2022-03-29 12:40:55 -0500

    sql where clause exercises

---
+++ where_exercises.sql
@@ -0,0 +1,17 @@
+use david;
+
+select * from employees
+where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya'
+and gender = 'M';
+
+select * from employees
+where last_name like 'E%' or last_name like '%E';
+
+select * from employees
+where last_name like 'E%' and last_name like '%E';
+
+select * from employees
+where last_name like '%q%';
+
+select * from employees
+where last_name like '%q%' and last_name not like '%qu%';
